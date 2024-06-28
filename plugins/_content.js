f44a22156f118c9482d1604076efe4ffcf40929e
TheElber7 <erubatomoushimasu@bol.com.br>
2024-06-27 22:43:03 -0300

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -9,9 +9,13 @@ handler.all = async function (m) {
 global.key = ''
 
 global.canales = [canal1, canal2, canal3, canal4].getRandom()
-global.welgata = [asistencia].getRandom()
-global.redesMenu = [canal1, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, asistencia].getRandom()
-global.accountsgb = [canal1, md, asistencia].getRandom()
+global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk, ths, asistencia].getRandom()
+global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
+global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
+
+global.canalIdGB = ["120363160031023229@newsletter", "120363169294281316@newsletter", "120363203805910750@newsletter", "120363302472386010@newsletter"]
+global.canalNombreGB = ["INFINITY-WA 💫", "GB - UPDATE 🐈", "Tips sobre GataBot 🤩", "NEW PROJECT: YartexBot-MD ✨"]
+global.channelRD = await getRandomChannel()
 
 // Imágenes 
 global.imagen1 = fs.readFileSync("./media/menus/Menu3.jpg")
